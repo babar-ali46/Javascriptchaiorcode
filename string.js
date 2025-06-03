512cd4faa53173f3fb0e51d8e3f354522b1e3125
unknown <babar-ali46>
2025-06-03 04:10:08 -0700

    first commit

---
+++ string.js
@@ -0,0 +1,10 @@
+//let A="Babar"
+//let B="Ali"
+//console.log( `first name ${A}  and last name ${B}`)
+
+
+ //******************************** */
+ const gameName= new String('hiteshhc')
+ console.log(gameName)
+ console.log(typeof[0
+ ])
---
+++ test.js
@@ -0,0 +1,12 @@
+//let score=true
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+//let valueinNumber=Number(score)
+//console.log(typeof valueinNumber);
+//console.log(valueinNumber);
+
+let  isLoggedin=1
+let booleanisloggedin=Boolean(isLoggedin)
+console.log(booleanisloggedin)
